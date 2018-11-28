e7debaddcd754c8a36b8b98e3f92dd8a1eb349c0
mayarals <limas.mayara@gmail.com>
2018-11-28 12:36:14 -0200

    remove file

--- aula-5/resource.js
+++ aula-5/resource.js
@@ -1,27 +1,78 @@
-//19.11.2018
-
 const express = require('express');
-const users = require('./users.js')
+const users = require('./users.js');
 const app = express();
+const Joi = require('joi');
+// const port = process.env.PORT || 3000;
 
+app.use(express.json());
 
-/*endpoint
-acessado pelo metodo get
-.send mostra para o usuario a res
-req: o que o usuario manda; res: o que manda pro usuário*/
+app.get('/', (req, res) => res.send('Hello world!'));
 
-app.get ('/', (req, res) => res.send('Hello World!'));
-app.get('api/users', (req, res) => res.send([1, 2, 3]));
-//app.get('api/users/:id', (req, res) => res.send(req.params));
-//app.get('api/user/:id', (req, res) => res.send(req.query));
-app.get('api/user/:id', (req, res) => {
-    const foundUser = users.find(user => user.id === parseInt(req.params.id)); //para encontrar o usuario
-    //res.send(foundUser); 
-    if (!foundUser){
-        return res.status(404).send('Deu merda');
-    }
-        res.send(foundUser);
+app.get('/api/users', (req, res) => res.send(users));
+
+app.get('/api/users/:id', (req, res) => {
+  const foundUser = users.find(user => user.id === parseInt(req.params.id));
+  if (!foundUser) {
+    return res.status(404).send('Deu merda');
+  }
+
+  res.send(foundUser);
 });
 
+app.post('/api/users', (req, res) => {
+  const id = Math.max(...users.map(user => user.id)) + 1;
+  const newUser = {
+    id,
+    name: req.body.name,
+    email: req.body.email
+  };
+  const schema = {
+    name: Joi.string().required().min(3),
+    email: Joi.string().required()
+  };
+  const validation = Joi.validate(req.body, schema);
 
-app.listen(3000, () => console.log('Escutando na porta 3000...'));
+  if (validation.error) {
+    return res.status(400).send(validation.error.details[0].message);
+  }
+
+  users.push(newUser);
+  res.send(newUser);
+});
+
+app.put('/api/users/:id', (req, res) => {
+  const foundUser = users.find(user => user.id === parseInt(req.params.id));
+
+  if (!foundUser) {
+    return res.status(404).send('Deu merda');
+  }
+
+  const schema = {
+    name: Joi.string().min(3).required(),
+    email: Joi.string().min(3).required(),
+  }
+  const validation = Joi.validate(req.body, schema);
+
+  if (validation.error) {
+    return res.status(400).send(validation.error.details[0].message);
+  }
+
+  foundUser.name = req.body.name;
+  foundUser.email = req.body.email;
+
+  res.send(foundUser);
+});
+
+app.delete('/api/users/:id', (req, res) => {
+  const foundUser = users.find(user => user.id === parseInt(req.params.id));
+
+  if (!foundUser) {
+    return res.status(404).send('Não foi possível encontrar o usuário');
+  }
+
+  users.splice(users.indexOf(foundUser), 1);
+  // users.delete(...);
+  res.send(foundUser);
+});
+
+app.listen(3000, () => console.log('Ouvindo na porta 3000...'));
--- aula-5/users.js
+++ aula-5/users.js
@@ -1,1 +1,19 @@
-module.exports.users
+const users = [
+    {
+      id: 1,
+      name: 'Anna Flávia',
+      email: 'annaflaviac@me.com'
+    },
+    {
+      id: 2,
+      name: 'Ana Paula',
+      email: 'ana.paula@reprograma.com.br'
+    },
+    {
+      id: 3,
+      name: 'Silvia',
+      email: 'silvia@reprograma.com.br'
+    }
+  ];
+  
+  module.exports = users;
