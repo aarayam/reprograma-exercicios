1cfba0c265e81d528aacc44065a9a22c7a382ebd
mayarals <limas.mayara@gmail.com>
2018-11-28 12:51:37 -0200

    criar API do postits

---
+++ aula-6/exercicio/postits.js
@@ -0,0 +1,27 @@
+const postits = [
+    {
+        id: 1,
+        titulo: 'Anna Flávia',
+        texto: 'annaflaviac@me.com'
+    },
+
+    {
+        id: 2,
+        titulo: 'Anna Flávia',
+        texto: 'annaflaviac@me.com'
+    },
+
+    {
+        id: 3,
+        titulo: 'Anna Flávia',
+        texto: 'annaflaviac@me.com'
+    },
+
+    {
+        id: 4,
+        titulo: 'Anna Flávia',
+        texto: 'annaflaviac@me.com'
+    }
+];
+
+module.exports = postits;
